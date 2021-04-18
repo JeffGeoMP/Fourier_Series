0e9d9bc7e3bce6186e15879eea81758273fb9d20
Jefferson Geovanny <jeffgeomp@gmail.com>
2021-04-18 03:34:34 -0600

    [update] Arreglo de Impresion por Consola

--- Prueba.py
+++ Prueba.py
@@ -1,16 +1,27 @@
 # Librerias Necesarios para Graficar
 import math 
 import numpy as np
+import sympy as sy
 from matplotlib import pyplot as plt 
 
+sy.init_printing(use_unicode = False, wrap_line = False)
+
 # Data del Grafico
-x = np.array(range(500))*0.1
-y = np.zeros(len(x))
-
-for i in range(len(x)):
-    y[i] = math.sin(x[i])
-
+#x = np.array(range(500))*0.1
+#y = np.zeros(len(x))
+#
+#for i in range(len(x)):
+#    y[i] = math.sin(x[i])
+#
 
 # Creamos el grafico
-plt.plot(x,y)
-plt.show()
+#plt.plot(x,y)
+#plt.show()
+
+x = sy.Symbol('x')
+
+integral = sy.integrate((x**2 + x + 1), x)
+print(sy.integrate((x**2 + x + 1), x))
+print(sy.sinc(0))
+
+sy.pprint(integral)
